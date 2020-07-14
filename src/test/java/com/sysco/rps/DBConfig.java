80d4951bb2d5d0ff2f7bf07821ba809ecb5ab741
Sanjaya Amarasinghe <sanjaya.amarasinghe@trycake.com>
2020-07-14 16:40:29 +0530

    Revert "Trying out embedded DBs"
    This reverts commit 09e18b2e996f90eb01335381832d188c0231c93b.

--- src/test/java/com/sysco/rps/DBConfig.java
+++ src/test/java/com/sysco/rps/DBConfig.java
@@ -1,12 +1,10 @@
 package com.sysco.rps;
 
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-
 /**
  * @author
  * @copyright (C) 2020,
  * @doc
  * @end Created : 14. Jul 2020 14:36
  */
-public class DBConfig extends DataSourceProperties {
+public class DBConfig {
 }
--- src/test/java/com/sysco/rps/TestConfig.java
+++ src/test/java/com/sysco/rps/TestConfig.java
@@ -1,14 +1,10 @@
 package com.sysco.rps;
 
-import org.springframework.boot.test.context.TestConfiguration;
-
 /**
  * @author
  * @copyright (C) 2020,
  * @doc
  * @end Created : 13. Jul 2020 16:55
  */
-
-@TestConfiguration
 public class TestConfig {
 }
