38843a75ff4d44c94722d1c221d0fa29e9d1eba1
Tharuka Jayalath <tharukajayalath@syscolabs.com>
2020-07-21 16:26:13 +0530

    Added util methods to loading data to db using csv

--- src/main/java/com/sysco/rps/repository/TestUtilsRepository.java
+++ src/main/java/com/sysco/rps/repository/TestUtilsRepository.java
@@ -6,10 +6,19 @@ import com.sysco.rps.entity.PriceZoneData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.data.r2dbc.core.DatabaseClient;
 import org.springframework.stereotype.Repository;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 
 /**
  * Contains some util methods that are only used during unit testing
@@ -94,6 +103,49 @@ public class TestUtilsRepository {
               })
               .subscribe();
         return isSuccess.get();
+    }
+
+    public void addPARecordsFromCsv(String fileName) {
+        generatePARecords(readCsv(fileName))
+              .forEach(this::addPARecord);
+    }
+
+    public void addPriceZoneRecordsFromCsv(String fileName) {
+        generatePriceZoneRecords(readCsv(fileName))
+              .forEach(this::addPriceZoneRecord);
+    }
+
+    private List<List<String>> readCsv(String fileName) {
+        ResourceLoader resourceLoader = new DefaultResourceLoader();
+        Resource[] scripts = new Resource[]{resourceLoader.getResource("classpath:".concat(fileName))};
+
+        List<List<String>> records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(scripts[0].getFile()))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                records.add(Arrays.asList(values));
+            }
+        } catch (Exception e) {
+            LOGGER.error("Error reading csv", e);
+        }
+        records.remove(0);
+        return records;
+    }
+
+    private List<PAData> generatePARecords(List<List<String>> csvData) {
+        return csvData
+              .stream()
+              .map(r -> new PAData(r.get(0), Integer.parseInt(r.get(1)), Double.parseDouble(r.get(2)),
+                    r.get(3), Long.parseLong(r.get(4)), r.get(5).charAt(0))
+              ).collect(Collectors.toList());
+    }
+
+    private List<PriceZoneData> generatePriceZoneRecords(List<List<String>> csvData) {
+        return csvData
+              .stream()
+              .map(r -> new PriceZoneData(r.get(0), Integer.parseInt(r.get(1)), r.get(2), r.get(3)))
+              .collect(Collectors.toList());
     }
 
 }
