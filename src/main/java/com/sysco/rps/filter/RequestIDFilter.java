854108278999530d0bcfa2758e1b41d76197f5d5
Tharuka Jayalath <tharukajayalath@syscolabs.com>
2020-09-23 11:46:02 +0530

    [PRCP-2323] Moved correlation id generation functionality to bounded elastic scheduler

--- src/main/java/com/sysco/rps/filter/RequestIDFilter.java
+++ src/main/java/com/sysco/rps/filter/RequestIDFilter.java
@@ -8,6 +8,7 @@ import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import reactor.util.context.Context;
 
 import java.util.List;
@@ -22,6 +23,7 @@ import static com.sysco.rps.common.Constants.CORRELATION_ID_KEY;
  *
  * @author Buddhika Karunatilleke
  * @author Sanjaya Amarasinghe
+ * @author Tharuka Jayalath
  * Date: 06/03/20
  */
 @Component
@@ -32,20 +34,24 @@ public class RequestIDFilter implements WebFilter {
     public Mono<Void> filter(ServerWebExchange serverWebExchange, WebFilterChain webFilterChain) {
 
         List<String> correlationIds = serverWebExchange.getRequest().getHeaders().get(CORRELATION_ID_HEADER);
-        String correlationId;
 
-        // if correlation ID is not there, create new one and append to MDC
         if (CollectionUtils.isEmpty(correlationIds)) {
-            correlationId = UUID.randomUUID().toString();
-        } else {
-            correlationId = correlationIds.get(0);
+            return Mono.defer(() -> Mono.just(UUID.randomUUID().toString()))
+                  .subscribeOn(Schedulers.boundedElastic())
+                  .publishOn(Schedulers.parallel())
+                  .flatMap(correlationId -> updateContextWithRequestIdentifiers(serverWebExchange, webFilterChain, correlationId));
         }
+        return updateContextWithRequestIdentifiers(serverWebExchange, webFilterChain, correlationIds.get(0));
+    }
 
+    private Mono<Void> updateContextWithRequestIdentifiers(ServerWebExchange serverWebExchange, WebFilterChain webFilterChain, String correlationId) {
         List<String> clientIds = serverWebExchange.getRequest().getHeaders().get(CLIENT_ID);
         final String clientId = CollectionUtils.isEmpty(clientIds) ? "" : clientIds.get(0);
 
         LOGGER.info("setting correlation ID: [{}], client ID: [{}]", correlationId, clientId);
 
-        return webFilterChain.filter(serverWebExchange).subscriberContext((Context context) -> context.put(CORRELATION_ID_KEY, correlationId).put(CLIENT_ID, clientId));
+        return webFilterChain
+              .filter(serverWebExchange)
+              .subscriberContext((Context context) -> context.put(CORRELATION_ID_KEY, correlationId).put(CLIENT_ID, clientId));
     }
 }
