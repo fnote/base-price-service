591d5ad143ec2cd5c58d73c36ad3b0dd85c7a84b
Meuru Muthuthanthri <meuru.muthuthanthri@trycake.com>
2020-10-23 14:02:10 +0530

    PRCP-2488 Added status endpoint

---
+++ src/main/java/com/sysco/rps/config/RPSInfoContributor.java
@@ -0,0 +1,36 @@
+package com.sysco.rps.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/***
+ * This class contributes to add custom information to the /status endpoint
+ */
+@Component
+public class RPSInfoContributor implements InfoContributor {
+
+    @Value("${active.business.units}")
+    private String activeBusinessUnits;
+
+    private Environment env;
+
+    public RPSInfoContributor(Environment env) {
+        this.env = env;
+    }
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        Map<String, String> infoAttributes = new HashMap<>();
+        infoAttributes.put("activeBusinessUnits", activeBusinessUnits);
+        infoAttributes.put("activeProfile", Arrays.toString(env.getActiveProfiles()));
+
+        builder.withDetail("configs", infoAttributes);
+    }
+}
