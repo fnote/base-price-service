bd59a2b09689a38e8eca401d1aaa2536b150f149
Sanjaya Amarasinghe <sanjaya.amarasinghe@trycake.com>
2020-12-22 09:49:25 +0530

    [PRCP-2952] Minor refactoring

--- src/main/java/com/sysco/rps/controller/AbstractController.java
+++ src/main/java/com/sysco/rps/controller/AbstractController.java
@@ -3,6 +3,8 @@ package com.sysco.rps.controller;
 import com.sysco.rps.common.Errors;
 import com.sysco.rps.dto.ErrorDTO;
 import com.sysco.rps.exceptions.RefPriceAPIException;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -69,7 +71,9 @@ public abstract class AbstractController {
     ResponseEntity<Mono<ErrorDTO>> handleResponseStatusException(ResponseStatusException e) {
         String correlationId = MDC.get(CORRELATION_ID_KEY);
 
-        LOGGER.error("ResponseStatusException occurred", e);
+        LOGGER.error("ResponseStatusException occurred {} {}",
+              ExceptionUtils.getMessage(e),
+              ExceptionUtils.getStackTrace(e));
 
         ErrorDTO errorDTO = new ErrorDTO(Errors.Codes.UNEXPECTED_ERROR, e.getStatus().getReasonPhrase(), correlationId);
 
@@ -88,7 +92,9 @@ public abstract class AbstractController {
     @ExceptionHandler(Exception.class)
     ResponseEntity<Mono<ErrorDTO>> handleUnknownException(Exception e) {
         String correlationId = MDC.get(CORRELATION_ID_KEY);
-        LOGGER.error("Unknown exception occurred", e);
+        LOGGER.error("Unknown exception occurred {} {}",
+              ExceptionUtils.getMessage(e),
+              ExceptionUtils.getStackTrace(e));
         Mono<ErrorDTO> errorDTOMono = Mono.defer(() -> {
             String message = this.messages.getMessage(format(ERROR_PLACEHOLDER, Errors.Codes.UNEXPECTED_ERROR), new Object[]{}, UNKNOWN_ERROR,
                     Locale.getDefault());
