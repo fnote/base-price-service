3bf41469d4e149be0a17eb365e5773319e774510
dach2960 <dushyanta.acharya@sysco.com>
2021-10-05 09:25:01 -0500

    [TRSP-271] removed junit4 implementation and dependency with lates junit

--- src/test/java/com/sysco/rps/misc/PriceZoneTableConfigReloaderTest.java
+++ src/test/java/com/sysco/rps/misc/PriceZoneTableConfigReloaderTest.java
@@ -3,16 +3,16 @@ package com.sysco.rps.misc;
 import com.sysco.rps.config.PriceZoneTableConfigReloader;
 import org.awaitility.Duration;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.awaitility.Awaitility.await;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class PriceZoneTableConfigReloaderTest {
 
